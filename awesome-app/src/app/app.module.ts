74f3a3e125447865156a59a75a1eff9298df074d
Anil Joseph <anil.jos@gmail.com>
2021-06-07 23:41:51 +0530

    Day 1

--- awesome-app/src/app/app.module.ts
+++ awesome-app/src/app/app.module.ts
@@ -5,16 +5,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DatabindingComponent } from './databinding/databinding.component';
 import {HelloComponent} from './hello/hello.component';
+import {RouterModule, Routes} from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+// http://localhost:4200/
+// http://localhost:4200/binding
+
+// / http://abc.com/ngApp/home
+
+const routes: Routes = [
+  {path: "home", component: HelloComponent},
+  {path: "binding", component: DatabindingComponent},
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "**", component: NotFoundComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent, 
     HelloComponent,
-    DatabindingComponent
+    DatabindingComponent,
+    NotFoundComponent
     
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
